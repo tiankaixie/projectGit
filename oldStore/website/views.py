d0c0ebdd894537df39d29f6d8b159b09e080d936
Tiankai Xie <tiankaixie@gmail.com>
2016-07-24 11:27:46 -0700

    fixed js bugs& and nav bar right side
    collapse js and dropdown js
    particular period nav bar added
    delete the pager and about

--- oldStore/website/views.py
+++ oldStore/website/views.py
@@ -9,42 +9,55 @@ import json
 #@cache_page(60 * 15)
 def index(request):
 	if not 'historycategory' in request.session:
-		#temp = list(get_list_or_404(History))
-		#print(temp)
 		historycategorytemp = serializers.serialize("json", History.objects.all())
-		#json.dumps(temp)
 		request.session["historycategory"] = historycategorytemp
 	historycategory =json.loads(request.session["historycategory"])
-	return render(request,'index.html',{"tittle": "HOME","historycategory":historycategory})
+	return render(request,'index.html',
+		{"tittle": "HOME",
+		"historycategory":historycategory
+		})
 
 #@cache_page(60 * 15)
 def historyList(request):
 	if not 'historycategory' in request.session:
-		temp = get_list_or_404(History)
-		historycategorytemp = json.dumps(temp)
+		historycategorytemp = serializers.serialize("json", History.objects.all())
 		request.session["historycategory"] = historycategorytemp
 	historycategory =json.loads(request.session["historycategory"])
-	return render(request,'historylist.html',{"tittle": "History","historycategory":historycategory})
+	return render(request,'historylist.html',
+		{"tittle": "History",
+		"historycategory":historycategory
+		})
 
 #@cache_page(60 * 15)
 def particularHistory(request,history_id):
 	if not 'historycategory' in request.session:
-		temp = get_list_or_404(History)
-		historycategorytemp = json.dump(temp)
+		historycategorytemp = serializers.serialize("json", History.objects.all())
 		request.session["historycategory"] = historycategorytemp
 	historycategory =json.loads(request.session["historycategory"])
 	history = get_object_or_404(History,pk = history_id)
 	periods = get_list_or_404(HistoryPeriod,belongTo_id = history_id)
-	return render(request,'particularHistory.html',{"tittle": history.name,"periods":periods,"history":history,"historycategory":historycategory})
+	return render(request,'particularHistory.html',
+		{"tittle": history.name,
+		"periods":periods,
+		"history":history,
+		"historycategory":historycategory})
 
 #@cache_page(60 * 15)
 def particularPeriod(request,period_id):
 	if 'historycategory' not in request.session:
-		temp = get_list_or_404(History)
-		historycategorytemp = json.dumps(temp)
+		historycategorytemp = serializers.serialize("json", History.objects.all())
 		request.session["historycategory"] = historycategorytemp
 	historycategory =json.loads(request.session["historycategory"])
 	period = get_object_or_404(HistoryPeriod,pk = period_id)
 	events = period.events.all()
-	print(period.events)
-	return render(request,'particularPeriod.html',{"tittle": period.name,"particularPeriod":period,"historycategory":historycategory,"events":events})
+	history = get_object_or_404(History,pk = period.belongTo_id)
+	periods = history.periods.all()
+	print(periods)
+	#print(period.events)
+	return render(request,'particularPeriod.html',
+		{"tittle": period.name,
+		"particularPeriod":period,
+		"historycategory":historycategory,
+		"events":events,
+		"periods":periods
+		})
